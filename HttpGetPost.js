9e5674e44def94bd6f12ed48103dea6e6ee50820
alexis m <alexismasevicius@gmail.com>
2021-07-24 16:46:59 -0300

    24-7

--- HttpGetPost.js
+++ HttpGetPost.js
@@ -26,8 +26,29 @@ fetch("https://jsonplaceholder.typicode.com/posts")
 
 //POST
 fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
+  method: "POST", ////METODO
   body: JSON.stringify({
+    //// body=donde nosotros mandamos la info-como objeto- del recurso que queremos crear
+    ///JSON.stringify convierrte un objeto javascript en un valor en formato JSON
+    title: "foo", ///// OBJETO-informacion
+    body: "bar",
+    userId: 1,
+  }),
+  headers: {
+    //// header= envia algun tipo de info especifica relacionada con el request (contenttype:aplicacion JSON, y si necesitariamos una autorizacion-token)
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+////PUT
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  ///LE indica mediante la direccion que recurso va a actualizar... en este caso el 1
+  method: "PUT",
+  body: JSON.stringify({
+    id: 1,
     title: "foo",
     body: "bar",
     userId: 1,
@@ -38,3 +59,10 @@ fetch("https://jsonplaceholder.typicode.com/posts", {
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+///DELETE
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  ///SOLO MANDA EL METODO
+  method: "DELETE",
+});
